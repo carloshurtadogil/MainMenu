fb7bbcd4922d65b29f562a415860c94ee78a26c8
Carlos Hurtado <carloshurtado219@gmail.com>
2018-11-30 18:07:41 -0800

    Unload 4

---
+++ Main/Assets/Emails.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emails : MonoBehaviour {
+
+    public string[] saved = new string[50];
+    int a = 0;
+
+    public void Addsave(string x)
+    {
+        saved[a] = x;
+        a++;
+    }
+
+
+    public string getsave(int i)
+    {
+        return saved[i];
+    }
+}
