f9ce677cdf7edf52cd5937ad164e87f2b7805ac3
Carlos Hurtado <carloshurtado219@gmail.com>
2018-11-30 17:55:45 -0800

    Upload 1

---
+++ Main/Assets/Addodatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Addodatabase : MonoBehaviour {
+
+    public InputField something;
+    private string word;
+    public Emails otherObject;
+    public void Setget()
+    {
+        something.text = word;
+        Debug.Log(word);
+        otherObject.Addsave(word);
+        something.text = "";
+    }
+    
+}
